098d08463ba3ce0015db01bc0c84bd494e377dae
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 10:39:50 -0400

    enhanced data

--- server.js
+++ server.js
@@ -9,12 +9,26 @@ app.listen(port, function(req, res){
     console.log("Here to save the city.")
 })
 
-const superheroes = ["Superman" , "Wonder Woman", "Black Panther"];
+const superheroes = [
+    {
+        name:"Superman",
+        powers: ["flight" , "invulnerability" , "x-ray vision"]
+                        } ,
+    {
+        name: "Wonder Woman" ,
+        powers: ["lasso of truth" , "strength", "speed"]
+                        },
+    {
+        name: "Black Panther",
+        powers: ["senses" , "reflexes", "healing"]
+                        }
+];
 
 app.get("/superheroes", function (req, res){
     res.send(superheroes)
 })
 
+// show route
 app.get("/superheroes/:index", function (req, res){
     res.send(superheroes[req.params.index])
 })
