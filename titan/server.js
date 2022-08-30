785053a006e4f7869ca10992e5c57f9f7237fb72
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 11:12:36 -0400

    titan index shows

--- titan/server.js
+++ titan/server.js
@@ -5,3 +5,28 @@ const port2 = 3333;
 app.listen(port2, function(){
     console.log("Protect the walls!")
 })
+
+const titan = [
+    {
+        name: "Eren",
+        location: "Shiganshina",
+        eyeColor: "brown",
+        hairColor: "brown",
+    },
+    {
+        name: "Armin",
+        location: "Wall Rose",
+        eyeColor: "blue",
+        hairColor: "blonde"
+    },
+    {
+        name: "Mikasa",
+        location: "Shiganshina",
+        eyeColor: "gray",
+        hairColor: "black",
+    },
+]
+
+app.get("/titan", function(req,res){
+    res.send(titan)
+})
