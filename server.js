07f52356ec722a3e2d6db6377c7e773da68fcaf3
Matt Porter <matthew.ellis.porter@gmail.com>
2022-08-30 10:53:15 -0400

    fixed villain data

--- server.js
+++ server.js
@@ -5,45 +5,54 @@ const express = require("express");
 const app = express();
 const port = 4444;
 
-app.listen(port, function(req, res){
+app.listen(port, function (req, res) {
     console.log("Here to save the city.")
 })
 
 const superheroes = [
     {
-        name:"Superman",
-        powers: ["flight" , "invulnerability" , "x-ray vision"]
-                        } ,
+        name: "Superman",
+        powers: ["flight", "invulnerability", "x-ray vision"]
+    },
     {
-        name: "Wonder Woman" ,
-        powers: ["lasso of truth" , "strength", "speed"]
-                        },
+        name: "Wonder Woman",
+        powers: ["lasso of truth", "strength", "speed"]
+    },
     {
         name: "Black Panther",
-        powers: ["senses" , "reflexes", "healing"]
-                        },
-        {
+        powers: ["senses", "reflexes", "healing"]
+    },
+];
+
+const villains = [
+    {
         name: "Lex Luthor",
         powers: ["super brain"],
-                          },
-     {
+    },
+    {
         name: "Ares",
         powers: ["shape shifting", "teleporting"],
-                          },
+    },
     {
         name: "Killmonger",
         powers: [
             "ability to fit in Black Panther's suit",
             "internet agreement that he looks badass",
-                            ],
-                          },
-];
+        ],
+    },
+]
 
-app.get("/superheroes", function (req, res){
+app.get("/superheroes", function (req, res) {
     res.send(superheroes)
 })
 
 // show route
-app.get("/superheroes/:index", function (req, res){
+app.get("/superheroes/:index", function (req, res) {
     res.send(`<h1>${superheroes[req.params.index].name}</h1> <br> <ul><li>${superheroes[req.params.index].powers[0]}</li><li>${superheroes[req.params.index].powers[1]}</li><li>${superheroes[req.params.index].powers[2]}</li></ul>`)
+})
+
+// show route
+
+app.get("/villains", function (req, res){
+    res.send(villains);
 })
